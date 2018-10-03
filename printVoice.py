6b16ca8313f21814d9b4fc2c3acc98799cd79fec
Esteban <esteban.9682@gmail.com>
2018-10-02 21:00:13 -0600

    commit esteban

--- printVoice.py
+++ printVoice.py
@@ -3,38 +3,63 @@ import webbrowser
 import string
 import speech_recognition as SR
 import os
+import time
+
+escuchando = True;
 
 # obtain audio 
 r = SR.Recognizer()
-with SR.Microphone() as source:
-    #print("Hello, say go \n or ##bye## for close!")
-    os.system("say 'Whats up?!, Do I work or not?'")
-    audio = r.listen(source)
 
-    if "bye" in r.recognize_google(audio):
-        os.system("say 'chao-chao, salshishon'")
-        exit()
+def comando(cadena):
+        print("hola")
+        cadena = cadena.lower()
+        entrada = cadena.split()
+        if(entrada[1] == "columnas"):
+                print(entrada[0])
+        elif(entrada[1] == "filas" or entrada[1] == "pilas"):
+                print(entrada[0])
+        elif(entrada[0] == "tamaño"):
+                print(entrada[1])
+        elif(entrada[0] == "inicio"):
+                print(entrada[1])
+        elif(entrada[0] == "final"):
+                print(entrada[1])
+        elif(entrada[0] == "limpiar"):
+                print(entrada[1])
+        
 
-    if "bacon" in r.recognize_google(audio):
-        os.system("say 'Hey you!, What i am going to print?'")
-        r5 = SR.Recognizer()
-        with SR.Microphone() as source:
-            audio5 = r5.listen(source)
-            try:
-                #print("Google Speech Recognition thinks you said \n\t\t\t" + r5.recognize_google(audio5))
-                os.system("say 'You said'")
-                print(r5.recognize_google(audio5))
-            except SR.UnknownValueError:
-                os.system("say 'Bacon could not understand you'")
-            except SR.RequestError as e:
-                os.system("say 'I could not request results from Google Speech Recognition service. Do you want error report?'")
+def escuchar():
+        global escuchando
+        global r
+        
+        while (escuchando == True):
+            time.sleep(1)
+            with SR.Microphone() as source:
+                os.system("say 'Esperando instrucción'")
                 
-                rc = SR.Recognizer()
-                with SR.Microphone() as source:
-                    audio = rc.listen(source)
-                    if "yes" in rc.recognize_google(audio):
-                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-                    if "no" in rc.recognize_google(audio):
-                        os.system("say 'so! do I'")
+                audio5 = r.listen(source)
+                try:
+                    audio = r.recognize_google(audio5,language = "es-CR")
+                    comando(audio)
+                    #print(audio)
+                except SR.UnknownValueError:
+                   os.system("say 'Bacon could not understand you'")
+                except SR.RequestError as e:
+                    os.system("say 'I could not request results from Google Speech Recognition service. Do you want error report?'")
+
+
+        
+                
+                    
+            
+        
+        
+    
+    
+
+
+
+
+
 
 
